17c223ac6335559203bdeb072adaa741a6dd3e7a
unknown <jincyrajeevanrk.com>
2024-10-12 20:31:03 +0530

    deployment files

--- churchapp/models.py
+++ churchapp/models.py
@@ -15,7 +15,7 @@ class NewsModel(models.Model):
     news_heading = models.CharField(max_length=200)
     main_image = models.ImageField(upload_to='images/')
     heading = models.CharField(max_length=100, null=True, blank=True)
-    description = models.CharField(max_length=5000, null=True, blank=True)  
+    description = models.TextField() 
     date = models.DateField(null=True,blank=True)
     def __str__(self):
         return self.news_heading
