e50320b7d4a913cf8d5c4eb0771db93bfde60c8e
tinoslabs <59659322+tinoslabs@users.noreply.github.com>
2024-10-12 12:18:28 +0530

    Update settings.py

--- churchpro/settings.py
+++ churchpro/settings.py
@@ -16,7 +16,7 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
